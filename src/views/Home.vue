693d327c378923dc922f5e919e77eeda2ea13ad8
pridekk <pridekk@gmail.com>
2021-12-17 07:26:40 +0900

    firebase functions 웹에서 분리

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,22 +6,13 @@
   </div>
 </template>
 
-<script lang="ts">
-import {computed, defineComponent, watch} from 'vue'
+<script lang="ts" setup>
+import { computed } from 'vue'
 import { useStore} from 'vuex'
-import { useRouter } from "vue-router";
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-  },
-  setup() {
-    const store = useStore()
-    const user = computed(() => store.state.user)
-    const authIsReady = computed(() => store.state.authIsReady)
-    const router = useRouter()
 
-    return { user }
-  }
-});
+const store = useStore()
+const user = computed(() => store.state.user)
+
+
 </script>
